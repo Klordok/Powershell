2a6422739014a7a8ac47f30fdec21c7efc386433
Klordok <klordok@gmail.com>
2018-08-08 17:02:03 -0700

    Add files via upload
    #Find the last time the user logged on to each DC
    #Show password and account lock state

---
+++ lastLogonPerDC.ps1
@@ -0,0 +1,24 @@
+﻿[cmdletbinding()]
+
+PARAM(
+    [Parameter(Mandatory=$true)]
+    [string]$username
+    )
+
+#Get list of all domain controllers
+$DCList = (Get-ADDomainController -Filter *).Name
+
+$DCLogonTimes = @()
+foreach($dc in $DCList){
+#Find the last time the user logged on to each DC
+    "Checking "+$dc 
+    $dcLastLogon = Get-ADUser -Identity $username | Get-ADObject -Server $DC -Properties lastLogon |
+    Select-Object @{Name='DC';Expression={$DC}}, @{Name='LastLogon';Expression={[datetime]::FromFileTime($_.lastlogon)}} -ErrorAction Stop
+
+    $DCLogonTimes += $dcLastLogon
+
+}
+Get-aduser -Identity $username -Properties LockedOut, PasswordExpired, PasswordLastSet | Select Name, LockedOut, PasswordExpired, PasswordLastSet
+
+#Show log on times for all DCs starting with most recent.
+$DCLogonTimes | Sort-Object LastLogon -Descending | FT -AutoSize
